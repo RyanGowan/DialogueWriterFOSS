a0f63d9d81d8d33febb2297de40edc48b277f085
Ryan Gowan <rygowan@gmail.com>
2025-07-31 18:06:59 -0300

    Added the ability to save and load
    Dialogue writer can now save a .txt or .json
    Dialogue writer can now load a .txt or .json

--- Scripts/InputToJSON.gd
+++ Scripts/InputToJSON.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var acceptBtn = $Background/Confirm/Buttons/Add;
 @onready var clearBtn = $Background/Confirm/Buttons/Delete;
 @onready var saveBtn = $Background/Output/Save;
+@onready var loadBtn = $Background/Output/Load;
 @onready var dialogueOptionBtn = $Background/InputScroll/InputContainer/AddDialogueOption;
 
 @onready var idField = $Background/InputScroll/InputContainer/IDInput;
@@ -13,13 +14,24 @@ extends Control
 
 @onready var outputField = $Background/Output/CodeEdit;
 
+@onready var saveFileDialog = $Background/SaveFileDialog;
+@onready var loadFileDialog = $Background/LoadFileDialog;
+
+var fileDialogPath: String = "";
+
 var currentContent: String = "";
 var currentEntries: Array = [];
 var currentCharacters: Array = [];
 
 func _ready():
 	acceptBtn.button_down.connect(AddToFile);
+	clearBtn.button_down.connect(ClearInputFields);
+	saveBtn.button_down.connect(SaveCurrentJSON);
+	loadBtn.button_down.connect(LoadDialogueFile);
 	pass;
+
+#region Add Entry Functions
+#returns a string representing which characters are present in the current dialogue file
 func UpdatePresentCharacters() -> String:
 	var returnVal = "";
 	var startLine = str("\"", "PresentCharacters", "\"", ":[");
@@ -34,7 +46,8 @@ func UpdatePresentCharacters() -> String:
 		pass;
 	returnVal = str(returnVal, "\n\t],",);
 	return returnVal;
-	
+
+#adds the current contents of the input fields to the JSON file
 func AddToFile() -> void:
 	var entry: String = GenerateEntry();
 	currentEntries.append(entry);
@@ -55,7 +68,9 @@ func AddToFile() -> void:
 	outputField.text = finalResult;
 	ClearInputFields();
 
+#generates a JSON entry from the data in the input fields
 func GenerateEntry() -> String:
+	
 	var finalString = "";
 	var idLine = str("\t\"", idField.text, "\"", ":{");
 	var dialogueLine = str("\t\t\"", "speech" , "\"", ":", "\"", dialogueField.text, "\"", ",");
@@ -71,6 +86,40 @@ func GenerateEntry() -> String:
 	
 	return finalString;
 	
+#endregion
+
+#region Save & Load Functions
+func SaveCurrentJSON() -> void:
+	saveFileDialog.show();
+	pass;
+	
+func LoadDialogueFile() -> void:
+	loadFileDialog.show();
+	pass;
+
+#event response functions rigged up to the save and load file dialogs, let us read and write to file
+func _on_save_file_dialog_file_selected(path):
+	fileDialogPath = path;
+	pass
+
+	
+func _on_save_file_dialog_confirmed():
+	var file = FileAccess.open(fileDialogPath, FileAccess.WRITE)
+	file.store_string(outputField.text);
+	file.close();
+	pass;
+	
+func _on_load_file_dialog_file_selected(path):
+	fileDialogPath = path;
+	
+func _on_load_file_dialog_confirmed():
+	var file = FileAccess.open(fileDialogPath, FileAccess.READ);
+	outputField.text = file.get_as_text();
+	file.close();
+	pass
+#endregion
+
+#resets all input fields to empty strings
 func ClearInputFields() -> void:
 	dialogueField.text = "";
 	idField.text = "";
